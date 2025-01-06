00deba25f68696b9264763335f5fee1707f9bd48
sandokhan <rhaydrick@gmail.com>
2025-01-06 21:17:18 +0000

    Solved exercise 2

--- exercises.py
+++ exercises.py
@@ -1,7 +1,18 @@
-quantity = int(input("Enter the quantity: "))
-price = float(input("Enter the price: "))
+# Exercise 1
+# quantity = int(input("Enter the quantity: "))
+# price = float(input("Enter the price: "))
 
-if quantity >=0 and price >= 0:
-    print("Data is valid")
+# if quantity >=0 and price >= 0:
+#     print("Data is valid")
+# else:
+#     print("Data is invalid")
+
+# Exercise 2
+temperature = float(input("Enter the temperature: "))
+
+if temperature < 18:
+    print("Low temperature")
+elif temperature >= 18 and temperature <= 26:
+    print("Normal temperature")
 else:
-    print("Data is invalid")
+    print("High temperature")
