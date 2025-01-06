2da2d61f9748aa127f4914477fadc1e7c9bc523d
sandokhan <rhaydrick@gmail.com>
2025-01-06 21:15:26 +0000

    Solved exercise 01

---
+++ exercises.py
@@ -0,0 +1,7 @@
+quantity = int(input("Enter the quantity: "))
+price = float(input("Enter the price: "))
+
+if quantity >=0 and price >= 0:
+    print("Data is valid")
+else:
+    print("Data is invalid")
